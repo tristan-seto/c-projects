eaced1f0be30475e5bf11626edfc09644c632d80
Tristan Seto <146482597+tristan-seto@users.noreply.github.com>
2023-11-07 18:27:45 -0500

    optimized for-loop search

--- three-digit-products.cpp
+++ three-digit-products.cpp
@@ -4,7 +4,7 @@
 // Date: 2023-11-07
 // Inspired by the following YouTube video: https://youtu.be/SpNxksyYMeA?si=Scpf4ZHDY8B9mjZ2
 // Prompt: Find all pairs of integer numbers {a, b, c} such that 2 < a * b * c < 7
-// Description: This program will find all the ordered pairs (a, b, c) such that the product satisfies the conditions imposed by the user.
+// This program will find all the ordered pairs (a, b, c) such that the product satisfies the conditions imposed by the user.
 
 int main(){
 
@@ -22,8 +22,8 @@ int main(){
     std::cout << std::endl << "Ordered pairs satisfying " << lower << " < abc < " << upper << ":" << std::endl;
 
     for(int a = 0; a < upper; a++){
-        for(int b = a; b < upper; b++){
-            for(int c = b; c < upper; c++){
+        for(int b = a; b < upper / a; b++){
+            for(int c = b; c < upper / (a * b); c++){
                 if(a * b * c > lower && a * b * c < upper){
                     std::cout << "(" << a << ", " << b << ", " << c << "): prod = " << a*b*c << std::endl;
                 }
